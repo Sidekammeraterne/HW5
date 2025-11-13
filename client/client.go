c0d43a3c61cd9f7f4f9a8d7b86f4f90e1ecb98c5
Marie Johansen <majoh@itu.dk>
2025-11-13 13:06:20 +0100

    Start to fill the structure
    Co-authored-by: Sara Bagger <github@saralouise.dk>

--- client/client.go
+++ client/client.go
@@ -1,1 +1,4 @@
 package client
+
+//needs to know who the leader server is (or maybe all)
+//sending bid(amount) to server
--- server/server.go
+++ server/server.go
@@ -1,1 +1,31 @@
 package server
+
+import (
+	proto "HW5/grpc/proto"
+	"time"
+)
+
+// the role of the server
+type Role int //todo: maybe not int?
+//we need N server nodes - min 2
+//one leader at a time
+
+// holding replicated data
+type AuctionState struct {
+	//todo: lock?
+	auctionStart time.Time //todo: what is Time
+	auctionEnd   time.Time
+
+	highestBid     int32
+	highestBidder  int32
+	registeredBids map[int32]bool //set bidderID to true?
+}
+
+//what needs to happen:
+//registered bidders, current highest bid and who made the bid
+//some kind of time, 100 time units, from starting the program
+
+//BACKUP: replicate state from the leader
+//if leader fails (figured out using timeout)?
+//backup promotes itself to leader
+//other backups now replicate the new leader
